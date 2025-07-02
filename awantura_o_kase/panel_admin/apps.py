dadd0435fdf1d1b4d317cb4261a5cad395aa1433
suchareq3 <wierzba41@gmail.com>
2025-07-03 00:44:32 +0200

    Usunięcie starego projektu

--- awantura_o_kase/panel_admin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class PanelAdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'panel_admin'
--- awantura_o_kase/panel_admin/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(required = True)
-    password = forms.CharField(required = True, widget=forms.PasswordInput)
